6267b99b6416bb0dc9303f8f717f5ff6cd0950aa
Ayushman Pyne <ayushman.pyne@gmail.com>
2023-04-30 17:42:29 +0530

    Update f-1.js
    correcting the timing of the preloader

--- js/f-1.js
+++ js/f-1.js
@@ -1,10 +1,8 @@
-
 var loader = document.getElementById("preloader");
 
 window.addEventListener("load" , setTimeout(function(){
   loader.style.display = "none";
-}, 3000))
-
+}, 6000))
 
 // function(){
 //   loader.style.display = "none";
