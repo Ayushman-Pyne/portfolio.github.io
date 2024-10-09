40f3ccf81cdb20403e021990d008c85cdcb52963
Ayushman Pyne <ayushman.pyne@gmail.com>
2024-10-09 14:48:12 +0530

    Update f-1.js

--- js/f-1.js
+++ js/f-1.js
@@ -2,6 +2,7 @@ var loader = document.getElementById("preloader");
 
 window.addEventListener("load" , setTimeout(function(){
   loader.style.display = "none";
+  mainContent.style.display = 'block';
 }, 6000))
 
 // function(){
